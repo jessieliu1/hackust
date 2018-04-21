c975bda54f3e32d2fd20b88d7d0e904e5cb731f1
Jessie Liu <jliu997@gmail.com>
2018-04-21 15:45:37 +0000

    add store parsing to parser

--- hackust/parser.py
+++ hackust/parser.py
@@ -24,7 +24,7 @@ class Receipt(object):
         """
 
         self.config = config
-        self.store = self.date = self.time = self.sum = None
+        self.store = self.date = self.time = self.total = None
         self.lines = raw_text
         self.sanitize()
         self.parse()
@@ -42,6 +42,9 @@ class Receipt(object):
 
         """
         self.date = self.parse_date()
+        self.time = self.parse_time()
+        self.store = self.parse_store()
+        self.total = self.parse_total()
 
     def fuzzy_find(self, keyword, accuracy=0.6):
         """
@@ -59,19 +62,71 @@ class Receipt(object):
         parses a variety of dates
         """
         date_formats = ['%m/%d/%Y', '%Y/%m/%d', '%d/%m/%Y', '%m/%d/%Y', '%Y/%m/%d', '%m/%d/%Y', '%Y.%m.%d', '%d.%m.%Y', '%m.%d.%Y', '%Y.%m.%d']
-        lines = ['11/24/1997'] #changed for testing
-        for line in lines: #changed for testing
-            for date_format in date_formats:
-                try:
-                    date = datetime.strptime(line, date_format)
-                except ValueError:
-                    pass
-                else:
-                    break
-        else:
-            date = None
-        print(date)
-        return date
+        #lines = ['11/24/1997'] #changed for testing
+        date = None
+        for line in self.lines: 
+            phrases = line.split()
+            for phrase in phrases:
+                for date_format in date_formats:
+                    try:
+                        phrase = phrase.strip()
+                        date = datetime.strptime(phrase, date_format)
+                        if date:
+                            break
+                    except ValueError:
+                        pass
+                    else:
+                        break
+        return date.date()
+
+    def parse_time(self):
+        time_formats = ['%H:%M:%S', '%H:%M', '%I:%M %p']
+        time = None
+        for line in self.lines: 
+            phrases = line.split()
+            for i in range(0, len(phrases)):
+                for time_format in time_formats:
+                    try:
+                        phrase = phrases[i].strip()
+                        if i < (len(phrases) - 1):
+                            phrase = phrases[i] + " " + phrases[i+1].strip()
+                            time = datetime.strptime(phrase, time_format)
+                            if time:
+                                break
+                        time = datetime.strptime(phrase, time_format)
+                        if time:
+                            break
+                    except ValueError:
+                        pass
+                    else:
+                        break
+        return time.time()
+
+    def parse_store(self):
+        for int_accuracy in range(10, 6, -1):
+            accuracy = int_accuracy / 10.0
+
+            for store, spellings in self.config.stores.items():
+                for spelling in spellings:
+                    line = self.fuzzy_find(spelling, accuracy)
+                    if line:
+                        return store
+
+    def parse_total(self):
+        for i in range(0, len(self.lines)):
+            for word in self.lines[i].split():
+                word = word.strip()
+                if word in self.config.sum_keys: 
+                    line = self.lines[i].replace(",", ".")
+                    sum_float = re.search(self.config.sum_format, line)
+                    if sum_float:
+                        return sum_float.group(0)
+                    elif (i < len(self.lines) - 1):
+                        sum_float = re.search(self.config.sum_format, self.lines[i+1])
+                        if sum_float:
+                            return sum_float.group(0)
+        return None
+
 
 def read_config(file="config.yml"):
         stream = open(os.path.join(os.getcwd(), file), "r")
@@ -90,7 +145,7 @@ def ocr_receipts(config, receipt_files):
     for receipt_path in receipt_files:
         with open(receipt_path) as receipt:
             receipt = Receipt(config, receipt.readlines())
-            print(receipt_path, receipt.date)
+            print(receipt_path, receipt.date, receipt.time, receipt.store, receipt.total)
 
 def main():
     config = read_config()
