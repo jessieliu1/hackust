debfa75e4ddeff7944d48f3e3fe2e49afd52362f
Jerry Meng <ymeng@umich.edu>
2018-04-21 14:20:52 +0800

    First commit, add Vagrantfile
    Environment can be consistent between all developers

---
+++ provision.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get -y install python3-venv python3-wheel python3-setuptools git tree
+sudo apt-get -y install default-jre python-pip
+
+sudo apt-get -y install sqlite3
