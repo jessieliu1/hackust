ed8cec282c18d489c0073d5efa16604314c9ecc5
Jerry Meng <ymeng@umich.edu>
2018-04-22 11:07:51 +0800

    lack of sleelp

--- hackust/js/receipt_list.jsx
+++ hackust/js/receipt_list.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Receipt from './receipt'
+import Receipt from './receipt';
+import $ from 'jquery';
+
 
 class ReceiptList extends React.Component {
   constructor(props) {
     super(props);
     this.render = this.render.bind(this);
     this.price_total = this.price_total.bind(this);
-    this.state = {receipts: []};
+    this.state = {receipts: [], num_show: 5};
+    this.setReceiptLimit = this.setReceiptLimit.bind(this);
   }
 
+  setReceiptLimit() {
+    this.setState({num_show: $("#num_receipts_select").val(), });
+  }
 
   componentDidMount() {
+    $('#receipts-submit').click((d) => {
+      this.setReceiptLimit();
+    });
     fetch('query/', { credentials: 'same-origin' })
       .then((response) => {
         if(!response.ok) throw Error(response.statusText);
@@ -33,14 +42,23 @@ class ReceiptList extends React.Component {
   }
 
   render() {
-    const receipts = this.state.receipts.map(p =>
+    let month_total = 0;
+    let days = Math.min(30, this.state.receipts.length);
+    /*this.state.receipts.slice(0, days).forEach(p => 
+      month_total += price_total(p); 
+        ); */
+    
+
+    const receipts = this.state.receipts.slice(0, this.state.num_show).map(p =>
         <Receipt key={p.receipt_id} name={p.store} price={this.price_total(p)}/>
         );
 
     return (
+        <div>
         <div id="receipts_list" className="container card card blue-grey lighten-5">
-          <h3>Past Receipts</h3>
-          {receipts}
+        <h3>Past Receipts</h3>
+        {receipts}
+        </div>
         </div>
         );
   };
